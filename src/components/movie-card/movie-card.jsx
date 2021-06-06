8652e8c4d55397f3d155993a4cfabb0fe8833b87
Veronika-Ro <79661110+Veronika-Ro@users.noreply.github.com>
2021-06-06 17:47:49 +0100

    Added Bootstrap Styling

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+<<<<<<< Updated upstream
+=======
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck';
+>>>>>>> Stashed changes
 
 export class MovieCard extends React.Component {
     render() {
         const { movie, onMovieClick } = this.props;
 
+<<<<<<< Updated upstream
         return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>;
+=======
+        return (
+            <CardDeck>
+                <Card border="dark">
+                    <Card.Img variant="top" src={movie.ImagePath} />
+                    <Card.Body>
+                        <Card.Title>{movie.Title}</Card.Title>
+                        <Card.Text>{movie.Description}</Card.Text>
+                        <Button onClick={() => onMovieClick(movie)} variant="link">Open</Button>
+                    </Card.Body>
+                </Card>
+            </CardDeck>
+        );
+>>>>>>> Stashed changes
     }
 }
 
