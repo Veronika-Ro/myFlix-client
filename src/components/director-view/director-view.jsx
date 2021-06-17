77adefb764e2fc24be947dc5032af45c2e3b3733
Veronika-Ro <79661110+Veronika-Ro@users.noreply.github.com>
2021-06-17 23:16:33 +0100

    director view updated and style added

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -8,33 +8,31 @@ import { Link } from "react-router-dom";
 export class DirectorView extends React.Component {
 
     render() {
-        const { movie, onBackClick } = this.props;
+        const { director, onBackClick } = this.props;
 
         return (
-            <Card>
+            <Card className="director-view">
+                <Card.Body>
 
-                <div className="director-view">
+                    <Card.Title className="director-name">
+                        <span className="value">{director.Name}</span>
+                    </Card.Title>
                     <ListGroup variant="flush">
-                        <ListGroup.Item className="director-name">
-                            <span className="value">{movie.Director.Name}</span>
-                        </ListGroup.Item>
                         <ListGroup.Item className="director-bio">
                             <span className="label">Bio: </span>
-                            <span className="value">{movie.Director.Bio}</span>
+                            <span className="value">{director.Bio}</span>
                         </ListGroup.Item>
                         <ListGroup.Item className="director-birth">
                             <span className="label">Birth: </span>
-                            <span className="value">{movie.Director.Birth}</span>
+                            <span className="value">{director.Birth}</span>
                         </ListGroup.Item>
                         <ListGroup.Item className="director-death">
                             <span className="label">Death: </span>
-                            <span className="value">{movie.Director.Death}</span>
+                            <span className="value">{director.Death}</span>
                         </ListGroup.Item>
                     </ListGroup>
-
-                </div>
-
-                <button onClick={() => { onBackClick(null); }}>Back</button>
+                </Card.Body>
+                <Button variant="outline-dark" size="sm" onClick={() => { onBackClick(null); }}>Back</Button>
             </Card >
 
         );
